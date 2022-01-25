0fb9cb7383534ebdcf106b5ca9278308a4eb7731
fallingskies <2421734257@qq.com>
2022-01-25 16:41:02 +0800

    完成二级目录，优化样式

--- src/main/java/com/firesword/controller/Basic_toolsController.java
+++ src/main/java/com/firesword/controller/Basic_toolsController.java
@@ -8,7 +8,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-public class MainController {
+public class Basic_toolsController {
     @FXML
     private VBox selectButton;
     @FXML
@@ -21,13 +21,13 @@ public class MainController {
                 refreshPage((node.getAccessibleText()));
             });
         }
-        refreshPage("Basic_tools");
+        refreshPage("shell");
     }
 
     private void refreshPage(String page){
         try {
             this.content.getChildren().clear();
-            AnchorPane contenPage = FXMLLoader.load(HelloApplication.class.getResource("/fxml/"+page+".fxml"));
+            AnchorPane contenPage = FXMLLoader.load(HelloApplication.class.getResource("/fxml/Basic_tools/"+page+".fxml"));
 
             this.content.getChildren().add(contenPage);
         }catch (Exception e){
